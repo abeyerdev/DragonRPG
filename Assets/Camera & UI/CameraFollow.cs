25eb00efe786121565b1128950ddd2e742cc44f7
Adam Beyer <abeyerdev@gmail.com>
2018-01-05 23:25:01 -0600

    initial checkin for DragonRPG

---
+++ Assets/Camera & UI/CameraFollow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    GameObject player;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        print(player.ToString());
+    }
+
+    // Called after other updates thus object positions should be finalized.
+    private void LateUpdate()
+    {
+        transform.position = player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
