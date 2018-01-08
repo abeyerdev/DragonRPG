d699fe6b4bd5e46b7256b6e89ee57f9f95845352
Adam Beyer <abeyerdev@gmail.com>
2018-01-08 00:32:34 -0600

    added basic health bars above enemies

---
+++ Assets/Enemies/Enemy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    private float currentHP = 100f;
+    [SerializeField]
+    float maxHP = 100f;
+
+    public float HealthAsPercentage
+    {
+        get
+        {
+            return currentHP / maxHP;
+        }
+    }
+}
