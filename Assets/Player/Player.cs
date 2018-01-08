2da76063c950e4b633f7a7a4ee1601d9fd3471c7
Adam Beyer <abeyerdev@gmail.com>
2018-01-07 23:36:03 -0600

    added player health bar

---
+++ Assets/Player/Player.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{    
+    private float currentHP = 100f;
+    [SerializeField]
+    float maxHP = 100f;
+
+    public float HealthAsPercentage
+    {
+        get
+        {
+            return currentHP / maxHP;
+        }
+    }
+}
